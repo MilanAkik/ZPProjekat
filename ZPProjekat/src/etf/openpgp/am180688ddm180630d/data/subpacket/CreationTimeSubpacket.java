18b87f9b2599de7fa2fa9dca1eba691e62f37c16
MilanAkik <mixa.mixony@gmail.com>
2022-06-08 02:34:57 +0200

    Made the first subpacket type

---
+++ ZPProjekat/src/etf/openpgp/am180688ddm180630d/data/subpacket/CreationTimeSubpacket.java
@@ -0,0 +1,29 @@
+package etf.openpgp.am180688ddm180630d.data.subpacket;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import etf.openpgp.am180688ddm180630d.data.enumerators.SignatureSubpacketType;
+
+public class CreationTimeSubpacket extends SignatureSubpacket {
+
+	protected LocalDateTime ldt;
+	public CreationTimeSubpacket(int length, SignatureSubpacketType type, LocalDateTime ldt) {
+		super(4, SignatureSubpacketType.SIGNATURE_CREATION_TIME);
+		this.ldt=ldt;
+	}
+	
+	@Override
+	public byte[] toByteArray()
+	{
+		byte[] arr = super.toByteArray();
+		int i=headerLength;
+		int uts = (int) ldt.atZone(ZoneId.systemDefault()).toEpochSecond();
+		arr[i++] = (byte) ((uts >> 24)&0xFF);
+		arr[i++] = (byte) ((uts >> 16)&0xFF);
+		arr[i++] = (byte) ((uts >> 8)&0xFF);
+		arr[i++] = (byte) (uts&0xFF);
+		return arr;
+	}
+	
+}
