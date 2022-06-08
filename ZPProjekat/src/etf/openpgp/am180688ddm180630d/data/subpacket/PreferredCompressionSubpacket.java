55f3812e72cbb600e9457758fdc54aa9dc7f2dbf
MilanAkik <mixa.mixony@gmail.com>
2022-06-08 11:21:37 +0200

    Added the preferred compression signature subpacket

---
+++ ZPProjekat/src/etf/openpgp/am180688ddm180630d/data/subpacket/PreferredCompressionSubpacket.java
@@ -0,0 +1,25 @@
+package etf.openpgp.am180688ddm180630d.data.subpacket;
+
+import etf.openpgp.am180688ddm180630d.data.enumerators.CompressionAlgorithm;
+import etf.openpgp.am180688ddm180630d.data.enumerators.SignatureSubpacketType;
+
+public class PreferredCompressionSubpacket extends SignatureSubpacket {
+
+	protected CompressionAlgorithm[] ca; 
+	public PreferredCompressionSubpacket(CompressionAlgorithm[] ca) {
+		super(ca.length+1, SignatureSubpacketType.PREFERRED_COMPRESSION_ALGORITHMS);
+	}
+	
+	@Override
+	public byte[] toByteArray()
+	{
+		byte[] arr = super.toByteArray();
+		int i=headerLength;
+		for(CompressionAlgorithm c: ca)
+		{
+			arr[i++] = (byte) c.getValue();
+		}
+		return arr;
+	}
+
+}
